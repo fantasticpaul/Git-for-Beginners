8767e241af93bdd5ded371e801e95109810ba437
fantasticpaul <fantasticpaul101@gmail.com>
2026-05-27 17:00:02 +0700

    added power function and updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from utils.calculator import add_nums, devide_nums, subtract_nums
+from utils.calculator import add_nums, devide_nums, subtract_nums, power
 
 
 if __name__ == "__main__":
     print(add_nums(2,3))
     print(devide_nums(10,2))
     print(subtract_nums(5,2))
+    print(power(2,3))
--- utils/calculator.py
+++ utils/calculator.py
@@ -6,3 +6,6 @@ def devide_nums(a,b):
 
 def subtract_nums(a,b):
     return a - b
+
+def power(a,b):
+    return a ** b
