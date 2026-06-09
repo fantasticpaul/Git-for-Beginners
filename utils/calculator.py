d5553a0f65d778c208658dfbc0f5a447941ef6b7
fantasticpaul <fantasticpaul101@gmail.com>
2026-06-09 14:59:56 +0700

    added docstring to calculator.py

--- utils/calculator.py
+++ utils/calculator.py
@@ -1,14 +1,19 @@
 def add_nums(a,b):
+    """This function adds two numbers together."""
     return a + b
 
-def devide_nums(a,b):
+def divide_nums(a,b):
+    """This function divides two numbers."""
     return a / b
 
 def subtract_nums(a,b):
+    """This function subtracts two numbers."""
     return a - b
 
 def power(a,b):
+    """This function raises a to the power of b."""
     return a ** b
 
 def multiply_nums(a,b):
-    return a *b
+    """This function multiplies two numbers."""
+    return a * b
